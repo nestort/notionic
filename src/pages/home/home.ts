aafcf78c0b8f8bf8825edf3a6308a3ae38bd4588
nestort <nestor.t.1992@gmail.com>
2018-04-20 18:01:09 -0500

    Creacion de servicios

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {NotasService} from '../../services/notas.service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  notas=[
-    {id:1,titulo:'titulo 1',descripcion:'descripcion de la nota 1'},
-    {id:2,titulo:'titulo 2',descripcion:'descripcion de la nota 2'},
-    {id:3,titulo:'titulo 3',descripcion:'descripcion de la nota 3'}
-  ];
-  constructor(public navCtrl: NavController) {
-
+  notas=[];
+  constructor(public navCtrl: NavController,public notasService:NotasService ) {
+    this.notas=notasService.getNotas();
+  }
+  public itemSelected(item:any){
+    
   }
 
 }
---
+++ src/services/notas.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class NotasService{
+    notas=[
+        {id:1,titulo:'titulo 1',descripcion:'descripcion de la nota 1'},
+        {id:2,titulo:'titulo 2',descripcion:'descripcion de la nota 2'},
+        {id:3,titulo:'titulo 3',descripcion:'descripcion de la nota 3'}
+      ];
+    public getNotas(){
+        return this.notas;
+    }
+}
