dc40a9cd91a85bd673a394c29d1ee89bfc2a54d7
nestort <nestor.t.1992@gmail.com>
2018-04-20 17:32:38 -0500

    Uso de lista simple

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  notas=[
+    {id:1,titulo:'titulo 1',descripcion:'descripcion de la nota 1'},
+    {id:2,titulo:'titulo 2',descripcion:'descripcion de la nota 2'},
+    {id:3,titulo:'titulo 3',descripcion:'descripcion de la nota 3'}
+  ];
   constructor(public navCtrl: NavController) {
 
   }
